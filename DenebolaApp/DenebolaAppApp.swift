8de78eaa00e3fafacc61bb23261c2dfe13424e51
Connor T <connor0530@gmail.com>
2021-05-01 21:18:03 -0400

    Initial Commit

---
+++ DenebolaApp/DenebolaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DenebolaAppApp.swift
+//  DenebolaApp
+//
+//  Created by Connor Tam on 5/1/21.
+//
+
+import SwiftUI
+
+@main
+struct DenebolaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
