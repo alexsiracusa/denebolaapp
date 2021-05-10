43890040142a87753e450d1e7e538751dc74dff0
Connor T <connor0530@gmail.com>
2021-05-10 17:42:00 -0400

    swift format, fix error

--- DenebolaApp/PodcastRSS/PodcastLoader.swift
+++ DenebolaApp/PodcastRSS/PodcastLoader.swift
@@ -8,30 +8,28 @@
 import Foundation
 
 class PodcastLoader: NSObject, ObservableObject, XMLParserDelegate {
-    
     let rss = "https://anchor.fm/s/f635e84/podcast/rss"
     @Published var podcasts: [Podcast] = []
-    var elementName: String = String()
+    var elementName = String()
     var title = String()
     var imageURL = String()
     var audioURL = String()
     var descrip = String()
-    
+
     func load() {
-        guard let url = URL(string: rss) else {return}
+        guard let url = URL(string: rss) else { return }
         podcasts = []
         loadFeed(url: url)
     }
-    
+
     private func loadFeed(url: URL) {
         let parser = XMLParser(contentsOf: url)!
         parser.delegate = self
         parser.parse()
     }
-    
-    // 1
-    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
 
+    // 1
+    func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String: String] = [:]) {
 //        if elementName == "item" {
 //            title = String()
 //            //imageURL = String()
@@ -52,8 +50,8 @@ class PodcastLoader: NSObject, ObservableObject, XMLParserDelegate {
     // 2
     func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
         if elementName == "item" {
-            let imageURL = URL(string: imageURL)
-            let audioURL = URL(string: audioURL)
+            let imageURL = URL(string: self.imageURL)
+            let audioURL = URL(string: self.audioURL)
             let podcast = Podcast(title: title, description: descrip, imageURL: imageURL, audioURL: audioURL)
             podcasts.append(podcast)
         }
@@ -63,23 +61,20 @@ class PodcastLoader: NSObject, ObservableObject, XMLParserDelegate {
     func parser(_ parser: XMLParser, foundCharacters string: String) {
         let data = string.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
 
-        if (!data.isEmpty) {
-            if self.elementName == "title" {
+        if !data.isEmpty {
+            if elementName == "title" {
                 title = data
-            } else if self.elementName == "description" {
+            } else if elementName == "description" {
                 descrip = data
             }
         }
     }
-    
 }
 
 struct Podcast: Identifiable {
-    var id: Int {title!.hash}
+    var id: Int { title!.hash }
     var title: String?
     var description: String?
     var imageURL: URL?
     var audioURL: URL?
 }
-
-
