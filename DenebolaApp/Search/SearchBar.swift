a6bd96a3585dad411fa38a3123fac8ae359434b7
Alex <alexander.siracusa@gmail.com>
2021-06-05 18:47:24 -0400

    cancel button for search bar.  Animation doesn't fully work

--- DenebolaApp/Search/SearchBar.swift
+++ DenebolaApp/Search/SearchBar.swift
@@ -10,28 +10,55 @@ import SwiftUI
 struct SearchBar: View {
     @State var searchFor: String = ""
     @Binding var updateSearch: String
+    @State var isEditing = false
     
     var body: some View {
-        ZStack {
+        HStack(spacing: 0) {
+            HStack(spacing: 0) {
+                TextField(
+                    "Search",
+                     text: $searchFor
+                ) { isEditing in
+                    withAnimation {
+                        self.isEditing = isEditing
+                    }
+                } onCommit: {
+                    updateSearch = searchFor
+                }
+            }
+            .background(
+                Rectangle()
+                    .foregroundColor(Color.gray)
+                    .brightness(0.37)
+                    .frame(height: 35)
+                    .cornerRadius(10)
+                    .padding([.leading, .trailing], -10)
+            )
+            if isEditing {
+                Button {
+                    searchFor = updateSearch
+                    UIApplication.shared.endEditing()
+                } label: {
+                    Text("Cancel")
+                        .foregroundColor(.blue)
+                }
+                .transition(.move(edge: .trailing))
+                .padding(.leading, 20)
+            }
+        }
+        .padding([.leading, .trailing], 20)
+        .background(
             Rectangle()
                 .fill(Color.white)
-                .frame(height: 40)
-            Rectangle()
-                .foregroundColor(Color.gray)
-                .brightness(0.35)
-                .frame(height: 30)
-                .cornerRadius(10)
-                .padding([.leading, .trailing], 10)
-            TextField(
-                "Search",
-                 text: $searchFor
-            ) { isEditing in
+                .frame(height: 50)
+        )
+        .frame(height: 50)
+    }
+}
 
-            } onCommit: {
-                updateSearch = searchFor
-            }
-            .padding([.leading, .trailing], 20)
-        }
+extension UIApplication {
+    func endEditing() {
+        sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
