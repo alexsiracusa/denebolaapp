af487eeb62e67848c29b74c4bb699b351bb1094d
Alex <alexander.siracusa@gmail.com>
2021-06-10 17:48:07 -0400

    added references in Retry settings

--- CypressApp/ServerAPI/RequestRetrySettings.swift
+++ CypressApp/ServerAPI/RequestRetrySettings.swift
@@ -8,6 +8,8 @@
 import Foundation
 import Alamofire
 
+/// https://github.com/Alamofire/Alamofire/blob/master/Source/RetryPolicy.swift
+
 class Retry: RetryPolicy {
     let delay: Double = 2.5
     let maxRetryCount: UInt = UInt.max
@@ -20,6 +22,22 @@ class Retry: RetryPolicy {
             completion(.retryWithDelay(delay))
         } else {
             completion(.doNotRetry)
+        }
+    }
+    
+    //not changed from original
+    override func shouldRetry(request: Request, dueTo error: Error) -> Bool {
+        guard let httpMethod = request.request?.method, retryableHTTPMethods.contains(httpMethod) else { return false }
+
+        if let statusCode = request.response?.statusCode, retryableHTTPStatusCodes.contains(statusCode) {
+            return true
+        } else {
+            let errorCode = (error as? URLError)?.code
+            let afErrorCode = (error.asAFError?.underlyingError as? URLError)?.code
+
+            guard let code = errorCode ?? afErrorCode else { return false }
+
+            return retryableURLErrorCodes.contains(code)
         }
     }
 }
