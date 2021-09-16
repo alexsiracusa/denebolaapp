1a2d6e2daaa26ed385f483d847ef800544cebdbe
Alex Siracusa <55099119+alexsiracusa@users.noreply.github.com>
2021-09-16 00:07:03 -0400

    Variable length loading row placeholders (#66)
    * random post row icons
    
    * random post card loader
    
    * SwiftFormat
    
    Co-authored-by: Sequal32 <Sequal32@users.noreply.github.com>

--- Denebola/MainViews/Feed/LoadingPostRowView.swift
+++ Denebola/MainViews/Feed/LoadingPostRowView.swift
@@ -8,33 +8,45 @@
 import SwiftUI
 
 struct LoadingPostRowView: View {
+    func longLength(_ width: CGFloat) -> CGFloat {
+        return CGFloat.random(in: (width / 2) ... width)
+    }
+
+    func shortLength(_ width: CGFloat) -> CGFloat {
+        return CGFloat.random(in: (width / 5) ... (width / 3))
+    }
+
     var body: some View {
         HStack(alignment: .top) {
             LoadingRectangle(10)
                 .frame(height: 90)
                 .aspectRatio(1.6, contentMode: .fit)
 
-            VStack(alignment: .leading, spacing: 0) {
-                LoadingRectangle()
-                    .frame(height: 11)
-                    .cornerRadius(10)
-                    .padding(.top, 7)
-                LoadingRectangle()
-                    .frame(height: 11)
-                    .cornerRadius(10)
-                    .padding(.top, 8)
-                    .padding(.trailing, 30)
-                LoadingRectangle()
-                    .frame(width: 50, height: 10)
-                    .cornerRadius(10)
-                    .padding(.top, 9)
-                LoadingRectangle()
-                    .frame(width: 100, height: 10)
-                    .cornerRadius(9)
-                    .padding(.top, 8)
-                Spacer()
+            GeometryReader { geo in
+                let width = geo.size.width
+
+                VStack(alignment: .leading, spacing: 0) {
+                    LoadingRectangle()
+                        .frame(width: longLength(width), height: 11)
+                        .cornerRadius(10)
+                        .padding(.top, 7)
+                    LoadingRectangle()
+                        .frame(width: longLength(width), height: 11)
+                        .cornerRadius(10)
+                        .padding(.top, 8)
+                        .padding(.trailing, 30)
+                    LoadingRectangle()
+                        .frame(width: shortLength(width), height: 10)
+                        .cornerRadius(10)
+                        .padding(.top, 9)
+                    LoadingRectangle()
+                        .frame(width: shortLength(width), height: 10)
+                        .cornerRadius(9)
+                        .padding(.top, 8)
+                    Spacer()
+                }
+                .foregroundColor(.black)
             }
-            .foregroundColor(.black)
             Spacer(minLength: 0)
         }
         .frame(height: 90)
@@ -45,6 +57,13 @@ struct LoadingPostRowView: View {
 
 struct LoadingPostRowView_Previews: PreviewProvider {
     static var previews: some View {
-        LoadingPostRowView()
+        VStack {
+            LoadingPostRowView()
+            LoadingPostRowView()
+            LoadingPostRowView()
+            LoadingPostRowView()
+            LoadingPostRowView()
+            LoadingPostRowView()
+        }
     }
 }
--- Denebola/MainViews/Home/SiteSection/LoadingPostCard.swift
+++ Denebola/MainViews/Home/SiteSection/LoadingPostCard.swift
@@ -8,6 +8,14 @@
 import SwiftUI
 
 struct LoadingPostCard: View {
+    func topLength(_ width: CGFloat) -> CGFloat {
+        return CGFloat.random(in: (width / 1.5) ... width)
+    }
+
+    func bottomLength(_ width: CGFloat) -> CGFloat {
+        return CGFloat.random(in: (width / 8) ... (width / 2))
+    }
+
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             LoadingRectangle()
@@ -15,15 +23,21 @@ struct LoadingPostCard: View {
                 .cornerRadius(10)
                 .frame(width: 220, height: 137.5)
 
-            VStack(alignment: .leading, spacing: 0) {
-                LoadingRectangle()
-                    .frame(width: 200, height: 10)
-                    .cornerRadius(10)
-                    .offset(y: -2)
-                    .padding(.bottom, 2)
-                LoadingRectangle()
-                    .frame(width: 100, height: 9)
-                    .cornerRadius(10)
+            GeometryReader { geo in
+                let width = geo.size.width
+
+                VStack(alignment: .leading, spacing: 0) {
+                    Spacer(minLength: 0)
+                    LoadingRectangle()
+                        .frame(width: topLength(width), height: 10)
+                        .cornerRadius(10)
+                        .offset(y: -2)
+                        .padding(.bottom, 2)
+                    LoadingRectangle()
+                        .frame(width: bottomLength(width), height: 9)
+                        .cornerRadius(10)
+                    Spacer(minLength: 0)
+                }
             }
             .frame(height: 36)
         }
@@ -34,6 +48,10 @@ struct LoadingPostCard: View {
 
 struct LoadingSmallPostCard_Previews: PreviewProvider {
     static var previews: some View {
-        LoadingPostCard()
+        VStack {
+            LoadingPostCard()
+            LoadingPostCard()
+            LoadingPostCard()
+        }
     }
 }
