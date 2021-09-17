5dbcfbbfd17e4eeb34ee9c1e810bad1141e9e59a
Alex Siracusa <55099119+alexsiracusa@users.noreply.github.com>
2021-09-16 23:03:17 -0400

    Reload in launch screen (#68)
    * can reload if loading the school somehow fails
    
    * Update Denebola/LaunchScreen/LaunchScreen.swift
    
    Co-authored-by: Connor T <33789660+Sequal32@users.noreply.github.com>
    
    * Update Denebola/LaunchScreen/LaunchScreen.swift
    
    Co-authored-by: Connor T <33789660+Sequal32@users.noreply.github.com>
    
    * fix dumb
    
    * SwiftFormat
    
    Co-authored-by: Connor T <33789660+Sequal32@users.noreply.github.com>
    Co-authored-by: Sequal32 <Sequal32@users.noreply.github.com>

--- Denebola/LaunchScreen/LaunchScreen.swift
+++ Denebola/LaunchScreen/LaunchScreen.swift
@@ -10,6 +10,26 @@ import SwiftUI
 struct LaunchScreen: View {
     @EnvironmentObject var viewModel: ViewModelData
     @State var showList = false
+    @State var error: Error? = nil
+
+    func load() {
+        viewModel.selectSchool().done {
+            if viewModel.school != nil {
+                viewModel.launching = false
+            } else {
+                if viewModel.schools == nil {
+                    viewModel.loadSchoolList().done { _ in
+                        showList = true
+                    }
+                } else {
+                    showList = true
+                }
+            }
+        }
+        .catch { error in
+            self.error = error
+        }
+    }
 
     var body: some View {
         Group {
@@ -27,35 +47,39 @@ struct LaunchScreen: View {
                     SchoolList()
                 }
             } else {
-                ZStack(alignment: .center) {
+                GeometryReader { geo in
                     Image("DenebolaLogoNoBackground")
                         .renderingMode(.original)
                         .aspectRatio(contentMode: .fit)
+                        .position(x: geo.size.width / 2, y: geo.size.height / 2)
                         .zIndex(1)
 
                     Color.white.ignoresSafeArea()
                         .zIndex(0)
+
+                    Group {
+                        if error != nil {
+                            Button {
+                                self.error = nil
+                                load()
+                            } label: {
+                                Image("reload")
+                                    .resizable()
+                                    .frame(width: 27, height: 27)
+                            }
+                        } else {
+                            SpinningLoader()
+                                .scaleEffect(1.33)
+                        }
+                    }
+                    .frame(width: 30, height: 30)
+                    .position(x: geo.size.width / 2, y: geo.size.height / 1.5)
                 }
                 .ignoresSafeArea()
             }
         }
         .onAppear {
-            viewModel.selectSchool().done {
-                if let _ = viewModel.school {
-                    viewModel.launching = false
-                } else {
-                    if viewModel.schools == nil {
-                        viewModel.loadSchoolList().done { _ in
-                            showList = true
-                        }
-                    } else {
-                        showList = true
-                    }
-                }
-            }
-            .catch { _ in
-                // do something idk
-            }
+            load()
         }
     }
 }
